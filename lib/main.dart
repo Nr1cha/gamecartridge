bd659a155e4a01516c18b53e2012e8399a7c3eae
Nr1cha <vrm.richards@gmail.com>
2025-05-21 12:07:46 -0600

    adding basic interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,22 +10,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World',
-                style: TextStyle(fontSize: 67, color: Colors.blue),
+      title: 'testing',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 45, 45, 45),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Header
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 60,
+            child: Container(
+              color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'BIG LOGO',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      TextButton(child: Text('Home'), onPressed: () {}),
+                      TextButton(child: Text('About'), onPressed: () {}),
+                      TextButton(child: Text('Contact'), onPressed: () {}),
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                'Welcome to flutter!',
-                style: TextStyle(fontSize: 24, color: Colors.green),
-              ),
-            ],
+            ),
           ),
-        ),
+          // Footer
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 40,
+            child: Container(
+              color: Colors.grey,
+              child: Center(
+                child: Text(
+                  'Copyright 2023',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          // Main Body Content
+          Positioned(
+            top: 60,
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  for (int i = 0; i < 20; i++)
+                    Card(
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            leading: Image.asset('assets/images/block.png'),
+                            title: const Text('Demo Title'),
+                            subtitle: const Text(
+                              'This is a simple card in Flutter',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ), 
+          ),
+        ],
       ),
     );
   }
